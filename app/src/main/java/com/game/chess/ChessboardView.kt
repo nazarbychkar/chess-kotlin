53986519d52dd622f30f06f4682e9fb801722191
chypa <mezismezis@gmail.com>
2024-03-07 15:56:01 +0200

    right measurements for the display size and some small refactoring

--- app/src/main/java/com/game/chess/ChessboardView.kt
+++ app/src/main/java/com/game/chess/ChessboardView.kt
@@ -1,26 +1,24 @@
 package com.game.chess
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
-import android.util.DisplayMetrics
 import android.view.View
 
-class ChessboardView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
+class ChessboardView(context: Context, attrs: AttributeSet?) : View(context, attrs) {
 //    private final val whiteColor = Color.rgb(255, 255, 240)
-    private final val whiteColor = Color.CYAN
-    private final val blackColor = Color.rgb(11, 1, 0)
+    private val whiteColor = Color.CYAN
+    private val blackColor = Color.rgb(11, 1, 0)
 
-    private val displayMetrics = DisplayMetrics()
+    private val displayMetrics = context.resources.displayMetrics
     private val height = displayMetrics.heightPixels
     private val width = displayMetrics.widthPixels
 
-    private final val sizeRect = 130f / (height * width)
-    private final val initialX = width - 20f
-    private final val initialY = (height / 2) + (4 * sizeRect)
+    private val sizeRect = width * 0.12f
+    private val initialX = (width / 2) - (sizeRect * 4)
+    private val initialY = (height / 2) - (sizeRect * 4)
 
     override fun onDraw(canvas: Canvas) {
         val paint = Paint()
